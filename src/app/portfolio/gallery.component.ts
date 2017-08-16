8922b1b0ab15cf099589e798ee10506a18bdd05e
Aaron Wallin <aaron.wallin@cudirect.com>
2017-08-16 14:23:21 -0700

    gallery update

--- src/app/portfolio/gallery.component.ts
+++ src/app/portfolio/gallery.component.ts
@@ -1,9 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html'
 })
-export class GalleryComponent { 
+export class GalleryComponent implements OnInit { 
   
+  ngOnInit(): void {
+    this.selectedGallery = this.galleryList[0];
+  }
+
+  selectedGallery: any = [];
+  galleryList: any = [
+    {      
+      id: 'events',
+      title: 'Events',
+      bgImagePath: 'assets/img/events/08.jpg',        
+      imageList: [
+        { imagePath: 'assets/img/events/01.jpg' },
+        { imagePath: 'assets/img/events/02.jpg' },
+        { imagePath: 'assets/img/events/03.jpg' },
+        { imagePath: 'assets/img/events/04.jpg' },
+        { imagePath: 'assets/img/events/05.jpg' },
+        { imagePath: 'assets/img/events/06.jpg' },
+        { imagePath: 'assets/img/events/07.jpg' },
+        { imagePath: 'assets/img/events/08.jpg' },
+        { imagePath: 'assets/img/events/09.jpg' },
+      ]
+    }
+  ];
 }
